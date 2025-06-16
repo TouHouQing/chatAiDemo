a67caecf27498df3a1ee4dbb201a0ff80089ec9e
touhouqing <qinghaoyang@foxmail.com>
2025-06-16 17:24:02 +0800

    init ChatController

---
+++ src/main/java/com/touhouqing/chataidemo/controller/ChatController.java
@@ -0,0 +1,23 @@
+package com.touhouqing.chataidemo.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/ai")
+public class ChatController {
+    private final ChatClient chatClient;
+
+    @RequestMapping(value="/chat", produces = "text/html;charset=utf-8")
+    public Flux<String> chat(String prompt){
+        return chatClient.prompt()
+                .user(prompt)
+                .stream()
+                .content();
+    }
+}
