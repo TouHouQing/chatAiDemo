f65a9237f2d208f3c4db3032515a40eadb9a78c6
touhouqing <qinghaoyang@foxmail.com>
2025-06-16 17:03:33 +0800

    init demo

---
+++ src/main/java/com/touhouqing/chataidemo/ChatAiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.touhouqing.chataidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatAiDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/touhouqing/chataidemo/ChatAiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.touhouqing.chataidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
