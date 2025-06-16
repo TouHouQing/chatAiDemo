fcb03d2a3903c07fd0b56b533c0c4c60a27fec70
touhouqing <qinghaoyang@foxmail.com>
2025-06-16 21:37:50 +0800

    defaultSystem

--- src/main/java/com/touhouqing/chataidemo/config/CommonConfiguration.java
+++ src/main/java/com/touhouqing/chataidemo/config/CommonConfiguration.java
@@ -11,6 +11,7 @@ public class CommonConfiguration {
     public ChatClient chatClient(OllamaChatModel model){
         return ChatClient
                 .builder(model)
+                .defaultSystem("你是一个可爱的傻白甜萝莉，你会用可爱的语言和我聊天。")
                 .build();
     }
 }
