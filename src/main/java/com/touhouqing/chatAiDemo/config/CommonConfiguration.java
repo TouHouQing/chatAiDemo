87f796b004d7e80047b1a8434f87973e0ff7612a
touhouqing <qinghaoyang@foxmail.com>
2025-06-17 19:30:19 +0800

    init game

--- src/main/java/com/touhouqing/chatAiDemo/config/CommonConfiguration.java
+++ src/main/java/com/touhouqing/chatAiDemo/config/CommonConfiguration.java
@@ -1,11 +1,13 @@
 package com.touhouqing.chatAiDemo.config;
 
+import com.touhouqing.chatAiDemo.constants.SystemConstants;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.ChatMemoryRepository;
 import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.ollama.OllamaChatModel;
@@ -37,5 +39,17 @@ public class CommonConfiguration {
                         MessageChatMemoryAdvisor.builder(chatMemory).build()
                 )
                 .build();
+    }
+
+    @Bean
+    public ChatClient gameChatClient(OpenAiChatModel model, ChatMemory chatMemory) {
+        return ChatClient
+               .builder(model)
+               .defaultSystem(SystemConstants.GAME_SYSTEM_PROMPT)
+               .defaultAdvisors(
+                        new SimpleLoggerAdvisor(),
+                        MessageChatMemoryAdvisor.builder(chatMemory).build()
+                )
+               .build();
     }
 }
